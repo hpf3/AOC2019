42ece183ca39535d74ef5437c3ccf54cc32935b7
hpf3 <18592150+hpf3@users.noreply.github.com>
2022-08-14 11:03:00 -0700

    structure setup

---
+++ guiApp/guiApp/structs/SolutionBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guiApp.structs
+{
+    public interface SolutionBase
+    {
+        string GetCreator();//return your name
+         string Solve(string input);//input will be raw text with /n and all
+    }
+}
