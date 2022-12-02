d966485fc3edb476addf67c279b939ab3f75c3b4
hpf3 <18592150+hpf3@users.noreply.github.com>
2022-12-02 11:13:33 -0800

    day 2 complete

--- guiApp/guiApp/Challenge Solutions/Solution2P2CB.cs
+++ guiApp/guiApp/Challenge Solutions/Solution2P2CB.cs
@@ -14,12 +14,32 @@ namespace guiApp.Challenge_Solutions
 
         public override string Solve(string input)
         {
-            throw new NotImplementedException();
+            Dictionary<string, int> Scores = MakeScores();
+            int Total = 0;
+            foreach (var round in input.Split("\n"))
+            {
+                Total += Scores.GetValueOrDefault(round);
+            }
+            return Total.ToString();
         }
 
-        public override void Test(string input)
+        private Dictionary<string, int> MakeScores()
         {
-            throw new NotImplementedException();
+            var Dict = new Dictionary<string, int>();
+            Dict.Clear();
+            Dict.Add("A X", (3 + 0));//rock,lose=scissors
+            Dict.Add("A Y", (1 + 3));//rock,draw=rock
+            Dict.Add("A Z", (2 + 6));//rock,win=paper
+
+            Dict.Add("B X", (1 + 0));//paper,lose=rock
+            Dict.Add("B Y", (2 + 3));//paper,draw=paper
+            Dict.Add("B Z", (3 + 6));//paper,win=scissors
+            
+            Dict.Add("C X", (2 + 0));//scissors,lose=paper
+            Dict.Add("C Y", (3 + 3));//scissors,draw=scissors
+            Dict.Add("C Z", (1 + 6));//scissors,win=rock
+            return Dict;
         }
+    
     }
 }
