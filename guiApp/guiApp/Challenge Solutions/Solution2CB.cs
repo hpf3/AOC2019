fe9d43de885f493fc69cde9a9a114e3b467f5099
hpf3 <18592150+hpf3@users.noreply.github.com>
2022-12-02 09:58:32 -0800

    Day 2 setup

---
+++ guiApp/guiApp/Challenge Solutions/Solution2CB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guiApp.Challenge_Solutions
+{
+    internal class Solution2CB : structs.SolutionBase
+    {
+        public override string Name => "Solution 2 CB";
+
+        public override string Creator => "CB";
+
+        public override string Solve(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
