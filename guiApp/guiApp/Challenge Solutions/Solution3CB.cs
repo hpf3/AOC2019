6d8d82f7d8928c634c9d1e9f037dcdf01242a155
hpf3 <18592150+hpf3@users.noreply.github.com>
2022-12-03 09:33:13 -0800

    Day 3 setup

---
+++ guiApp/guiApp/Challenge Solutions/Solution3CB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guiApp.Challenge_Solutions
+{
+    internal class Solution3CB : structs.SolutionBase
+    {
+        public override string Name => "Day 3 Part 1 CB";
+
+        public override string Creator => "CB";
+
+        public override string Solve(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
