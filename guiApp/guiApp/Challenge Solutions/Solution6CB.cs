83321b3f72707a7211b23e60d19ba9af01192151
hpf3 <18592150+hpf3@users.noreply.github.com>
2022-12-11 23:42:09 -0800

    day 6 complete due to part 2 being such a minor change, it has been moved to the test function

---
+++ guiApp/guiApp/Challenge Solutions/Solution6CB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guiApp.Challenge_Solutions
+{
+    internal class Solution6CB : structs.SolutionBase
+    {
+        public override string Name => "Day 6 Part 1 CB";
+
+        public override string Creator => "CB";
+
+        public override string Solve(string input)
+        {
+            return get_unique(input, 4);
+            
+        }
+        private string get_unique(string input,int size)
+        {
+            char[] chars = new char[size];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i > 3 && chars.Distinct().Count() == size)
+                {
+                    return (i).ToString();
+                }
+                chars[i % size] = input[i];
+            }
+            return $"Failed to find unique sequence of {size}";
+        }
+        public override void Test(string input)
+        {
+            Windows.UI.Popups.MessageDialog dialog = new Windows.UI.Popups.MessageDialog(get_unique(input, 6));
+            dialog.ShowAsync();
+        }
+    }
+}
